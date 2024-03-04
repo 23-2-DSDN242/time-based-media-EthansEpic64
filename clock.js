fb6edc340e503ff6320fc1dcfd4c59a6458aa998
EthansEpic64 <131923976+EthansEpic64@users.noreply.github.com>
2024-03-05 11:18:36 +1300

    started clock

--- clock.js
+++ clock.js
@@ -11,18 +11,29 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+  angleMode(DEGREES);
+  background(124, 152, 196); //  sky grey blue
+  translate(width/2, height/2);
+  let smoothrotate = lerp(obj.seconds, obj.seconds+1, 0.5)
 
+  //RPM gauge (seconds)
+  stroke(80); //grey
+  strokeWeight(6);
+  fill(40); //very dark grey
+  ellipse(300,0,230,230);
 
-  fill(249, 140, 255);// pink
-  ellipse(width / 3, 350, 150);
-  fill(140, 255, 251) // blue
-  ellipse(width / 2, 350, 150);
-  fill(175, 133, 255); // purple
-  ellipse(width / 3 * 2, 350, 150);
+  //KMH gauge (minutes)
+  ellipse(0,0,260,260)
+  
+  //needle (seconds)
+  push();
+  noStroke();
+  fill(225,0,0); //red
+  rotate(360/-60*smoothrotate);
+  rect(0,0,110,10);
+  pop();
 
+  noStroke();
+  fill(80);
+  ellipse(0,0,25,25)
 }
