fc2290cc30ce2525bd9572e69c73ae5d84224949
EthansEpic64 <hpethan64@gmail.com>
2024-03-04 19:58:08 +1300

    Update maeda_clock.js

--- maeda_clock.js
+++ maeda_clock.js
@@ -1,9 +1,36 @@
 // Update this function to draw you own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
+  angleMode(DEGREES);
+  background(200);
+  fill(0); // dark grey
+  textSize(400);
   textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+  translate(width/2, height/2);
+  push();
+  rotate(360/12*6);
+  rect(-50,-150,50,50);
+  rect(0,-150,50,50);
+  rect(50,-150,50,50);
+  rect(-100,-150,50,50);
+  rect(-100,-100,50,300);
+  rect(-50,150,50,50);
+  rect(0,150,50,50);
+  rect(50,200,50,-200);
+  rect(-50,0,100,50);
+  pop();
+
+  push();
+  fill(255,0,0);
+  rotate(360/60*5);
+  rect(0,-50,50,50);
+  rect(-50,-50,50,50);
+  rect(-100,-50,50,50);
+  rect(-100,-100,50,50);
+  rect(-100,-150,50,50);
+  rect(-50,-150,50,50);
+  rect(0,-150,50,50);
+  rect(0,0,50,50);
+  rect(-100,50,150,50);
+  pop();
 }
